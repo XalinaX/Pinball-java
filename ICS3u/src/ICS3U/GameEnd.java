ef16fda2fb47f2a33927eb569fa7e07c6ee55546
XalinaX <XalinaX@users.noreply.github.com>
2019-06-11 09:26:12 -0400

    hh

--- ICS3u/src/ICS3U/GameEnd.java
+++ ICS3u/src/ICS3U/GameEnd.java
@@ -12,12 +12,12 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.geom.Polygon;
-import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 /**
- * 	the game end
+ *  the game end
+ *  this class is called when the player loses 
  */
 public class GameEnd extends BasicGameState {
 	public static String score;
@@ -39,12 +39,11 @@ public class GameEnd extends BasicGameState {
 	public static boolean clicked;
 	
 	public GameEnd(int i) {
-//		score = (String)Game.userScore;
 	}
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame arg1) throws SlickException {
-		// TODO Auto-generated method stub
+		// initialize components
 		newgame = new Image("./image/newgame.png");
 		rankk = new Image("./image/rankk.png");
 		awtFont = new java.awt.Font("Arial", java.awt.Font.BOLD, 50);
@@ -54,6 +53,7 @@ public class GameEnd extends BasicGameState {
 		poly.addPoint(65, 55);
 		poly.addPoint(55, 45);
 		
+		// animation pics
 		animation = new Animation(true);
 		Image poke1 = new Image("./image/gameend/pokemon1.png");
 		Image poke2 = new Image("./image/gameend/pokemon2.png");
@@ -83,6 +83,7 @@ public class GameEnd extends BasicGameState {
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame arg1, Graphics g) throws SlickException {
+		// drawing methods
 		// user's score
 		font.drawString(150,150,"YOUR SCORE IS:",new Color(255,240,32));
 		font.drawString(360-score.length()*15,340,score,new Color(255,215,0));
@@ -92,8 +93,9 @@ public class GameEnd extends BasicGameState {
 		rankk.draw(100,770,500,90);
 		//animation
 		animation.draw((float) (picposition/3),990,120,120);
-		
+		// exit
 		exit.draw(640,0,60,60);
+		// mute button
 		if (soundOn) {
 			on.draw(0,0,60,60);
 		}else {
@@ -105,17 +107,19 @@ public class GameEnd extends BasicGameState {
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int arg2) throws SlickException {
+		// position of animation
 		picposition++;
 		if(picposition>2200)
 			picposition=0;
 		input = gc.getInput();
 		int xpos=Mouse.getX();
 		int ypos=1200-Mouse.getY();
+		// check any button is clicked
 		boolean clicked1=input.isMousePressed(Input.MOUSE_LEFT_BUTTON);
-		//all balls are at the top of the screen
 		if (clicked1&&ypos>0 && ypos<60&&((xpos>0&&xpos<270)||(xpos>640&&xpos<700))) {
 			clicked=true;
 		}else if (!Mouse.isButtonDown(0)&&clicked&& ypos>0 && ypos<60) {
+			// exit button
 			if (xpos>640 && xpos<700) {
 				gc.exit();
 				try {
@@ -124,25 +128,30 @@ public class GameEnd extends BasicGameState {
 					e.printStackTrace();
 				}
 			}else if (xpos>0 && xpos<60){
+				// mute button
 				if (soundOn) {
 					soundOn=false;
 				}else {
 					soundOn=true;
 				}
 			}else if (xpos>70 && xpos<130) {
+				// leaderboard button
 				Leaderboard.lastState = PinBall.gameend;
 				sbg.getState(PinBall.leaderboard).init(gc, sbg);
 				sbg.enterState(PinBall.leaderboard);
 			}else if (xpos>140 && xpos<200) {
+				// restart a new game button
 				sbg.enterState(PinBall.login);
 			}
 			clicked=false;
 		}
 		if (clicked1) {
 			if (Mouse.getX() >= 200 && Mouse.getX() <= 500 && 1200-Mouse.getY() <= 640 && 1200-Mouse.getY() >= 550) {
+				// start a new game button (on the middle)
 				sbg.getState(PinBall.game).init(gc, sbg);
 				sbg.enterState(PinBall.game);
 			} else if (Mouse.getX() >= 100 && Mouse.getX() <= 600 && 1200-Mouse.getY() <= 840 && 1200-Mouse.getY() >= 750) {
+				// looking at ranking button
 				Leaderboard.lastState = PinBall.gameend;
 				sbg.getState(PinBall.leaderboard).init(gc, sbg);
 				sbg.enterState(PinBall.leaderboard);
